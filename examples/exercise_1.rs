170fb145fc90d3e8f26022a5d2bff2c40913c824
topanisto <ankaa@mit.edu>
2023-06-13 17:13:54 -0400

    exercise_1 without direct function calls

--- examples/exercise_1.rs
+++ examples/exercise_1.rs
@@ -33,16 +33,45 @@ fn div32<F: ScalarField>(
     let lookup_bits =
         var("LOOKUP_BITS").unwrap_or_else(|_| panic!("LOOKUP_BITS not set")).parse().unwrap();
 
-    let range_bits = 16;
+    let range_bits: usize = 16;
     let range: RangeChip<F> = RangeChip::default(lookup_bits);
 
     // check r is 16-bit
     range.range_check(ctx, x, range_bits);
     println!("x is {} bits!", range_bits);
 
-    let (mod32, _rem) = range.div_mod(ctx, x, 32_u64, range_bits);
+    let mut x_bitw = range.gate().num_to_bits(ctx, x, range_bits);
 
-    println!("mod_32: {:?}", mod32.value());
+
+    let mut cumsum = ctx.load_witness(F::zero());
+
+
+    let two = Constant(F::from(2));
+    let last_power = range.gate().sub(ctx, Constant(F::from(16)), Constant(F::from(5)));
+
+    for pow in 0..16 {
+        let cur_const = x_bitw.pop().unwrap();
+        // going down the vector backwards
+
+        let idx = ctx.load_witness(F::from(pow));
+        let is_quotient = range.is_less_than(ctx, idx, last_power, range_bits);
+        // since bits are popped backwards, we check if we are still in the bracket for 2^k, where k>5
+
+        let multiplier = range.gate().select(ctx, Constant(F::one()), Constant(F::zero()), is_quotient); //const multiplier
+        let power_of_two = range.gate().select(ctx, two, Constant(F::one()), is_quotient);
+        // our output is either 2*cumsum + cur_const or 1*cumsum + 0
+
+        let cur_zeroth = range.gate().mul(ctx, multiplier, cur_const);
+        let new = range.gate().mul_add(ctx, power_of_two, cumsum, cur_zeroth);
+        cumsum = new;
+    }
+
+    make_public.push(cumsum);
+
+    println!("final: {:?}", cumsum.value());
+
+
+    // println!("mod_32: {:?}", mod32.value());
 
 }
 
