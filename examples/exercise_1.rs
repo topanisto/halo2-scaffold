eaef6558e2a5f7f7a7a94d12184b395b82a3bb00
topanisto <ankaa@mit.edu>
2023-06-14 18:33:17 -0400

    exercise_1 improvements

--- examples/exercise_1.rs
+++ examples/exercise_1.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use halo2_base::gates::{GateInstructions, RangeChip, RangeInstructions};
 use halo2_base::halo2_proofs::arithmetic::compute_inner_product;
+use halo2_base::halo2_proofs::plonk::Assigned;
 use halo2_base::utils::ScalarField;
 use halo2_base::{AssignedValue, QuantumCell};
 #[allow(unused_imports)]
@@ -33,45 +34,63 @@ fn div32<F: ScalarField>(
     let lookup_bits =
         var("LOOKUP_BITS").unwrap_or_else(|_| panic!("LOOKUP_BITS not set")).parse().unwrap();
 
-    let range_bits: usize = 16;
+    let num_bits: usize = 16;
     let range: RangeChip<F> = RangeChip::default(lookup_bits);
 
     // check r is 16-bit
-    range.range_check(ctx, x, range_bits);
-    println!("x is {} bits!", range_bits);
+    range.range_check(ctx, x, num_bits);
+    println!("x is {} bits!", num_bits);
 
-    let mut x_bitw = range.gate().num_to_bits(ctx, x, range_bits);
+    //without num_to_bits-- keep on subtracting powers of 2?
 
+    let mut x_rem = range.gate().add(ctx, Constant(F::zero()), x);
+
+    let mut pow2 = (5..16).map(|i| Constant(range.gate().pow_of_two()[i])).collect::<Vec<QuantumCell<F>>>();
+
+    for _ in 0..11 {
+        let cur_pow = pow2.pop().unwrap();
+        let valid_sub = range.is_less_than(ctx, x_rem, cur_pow, num_bits);
+        let sub_val = range.gate().select(ctx, Constant(F::zero()), cur_pow, valid_sub);
+        x_rem = range.gate().sub(ctx, x_rem, sub_val);
+    };
+
+    let x_quot = range.gate().sub(ctx, x, x_rem);
+
+    let x_out = range.gate().div_unsafe(ctx, x_quot, Constant(F::from(32)));
+
+    make_public.push(x_out);
+    print!("quotient: {:?}", x_out.value());
+
+
+
+
+    // num_to_bits implementations
+
+    let mut x_bitw = range.gate().num_to_bits(ctx, x, num_bits);
 
     let mut cumsum = ctx.load_witness(F::zero());
 
-
     let two = Constant(F::from(2));
-    let last_power = range.gate().sub(ctx, Constant(F::from(16)), Constant(F::from(5)));
 
-    for pow in 0..16 {
+    for _ in 0..11 {
         let cur_const = x_bitw.pop().unwrap();
         // going down the vector backwards
 
-        let idx = ctx.load_witness(F::from(pow));
-        let is_quotient = range.is_less_than(ctx, idx, last_power, range_bits);
         // since bits are popped backwards, we check if we are still in the bracket for 2^k, where k>5
-
-        let multiplier = range.gate().select(ctx, Constant(F::one()), Constant(F::zero()), is_quotient); //const multiplier
-        let power_of_two = range.gate().select(ctx, two, Constant(F::one()), is_quotient);
         // our output is either 2*cumsum + cur_const or 1*cumsum + 0
-
-        let cur_zeroth = range.gate().mul(ctx, multiplier, cur_const);
-        let new = range.gate().mul_add(ctx, power_of_two, cumsum, cur_zeroth);
+        let new = range.gate().mul_add(ctx, two, cumsum, cur_const);
         cumsum = new;
-    }
+    };
 
-    make_public.push(cumsum);
+    // num_to_bits without loops
 
-    println!("final: {:?}", cumsum.value());
+    let mut fe = (0..5).map(|_| Constant(F::zero())).collect::<Vec<QuantumCell<F>>>();
+    fe.extend((5..16).map(|i| Constant(range.gate().pow_of_two()[i])).collect::<Vec<QuantumCell<F>>>());
 
+    let _cumsum = range.gate().inner_product(ctx, x_bitw, fe);
 
-    // println!("mod_32: {:?}", mod32.value());
+    // make_public.push(cumsum);
+    // println!("final: {:?}", cumsum.value());
 
 }
 
