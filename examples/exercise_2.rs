5ef7fd182f010c35e748d17d0f01b6cd9ad9e47b
topanisto <ankaa@mit.edu>
2023-06-19 12:39:02 -0400

    random access

--- examples/exercise_2.rs
+++ examples/exercise_2.rs
@@ -54,63 +54,33 @@ fn some_algorithm_in_zk<F: ScalarField>(
 
     //pubic params made public
 
-    let start_from_end = range_gate.gate().sub(ctx, Constant(F::from(999)), start);
-    let end_from_end = range_gate.gate().sub(ctx, Constant(F::from(999)), end);
-
-    let mut up_working_arr: Vec<AssignedValue<F>> = Vec::new();
-    up_working_arr.extend(&base_arr); 
-
-    //set all out of range elements to 0
     let mut working_arr: Vec<AssignedValue<F>> = Vec::new();
+    working_arr.extend(&base_arr); 
     working_arr.push(ctx.load_zero());
 
-    for idx in 0..1000 {
-        let cur = up_working_arr.pop().unwrap();
+    for idx in 0u64..1000 {
+        let cur = working_arr[idx as usize];
+
         let cur_idx = Constant(F::from(idx));
-        let left_range = range_gate.is_less_than(ctx, cur_idx, end_from_end, range_bits);
-        let right_range = range_gate.is_less_than(ctx, start_from_end, cur_idx, range_bits);
+        let left_range = range_gate.is_less_than(ctx, cur_idx, start, range_bits);
+        let right_range = range_gate.is_less_than(ctx, end, cur_idx, range_bits);
         let not_in_selected = range_gate.gate().or(ctx, left_range, right_range); 
         let pushed = range_gate.gate().select(ctx, assigned_elt, cur, not_in_selected);
-        working_arr.push(pushed);
+
+        working_arr[idx as usize] = pushed;
     }
-    working_arr.push(ctx.load_zero());
 
-    let mut itr_parity = ctx.load_zero();
-
-    // [0, a999, a998, ..., a0, 0], 1002 elts
-
-    // rotate the vector to the left by 1 *start number of times
 
     for itr in 0..1000{
-        itr_parity = range_gate.gate().not(ctx, itr_parity); //starts on 1, removes need for list reversals
-        let mut shift_arr: Vec<AssignedValue<F>> = Vec::new(); //vec of length 1000, to hold the running shift array
 
         let cur_iter = ctx.load_witness(F::from(itr));
-        let not_rotated_enough = range_gate.is_less_than(ctx, cur_iter, start, range_bits); //are we done rotating ?
-
-        for _idx in 0..1000 {
-            let elt_n = working_arr.pop().unwrap();
-            let elt_c = working_arr.pop().unwrap();
-            let elt_p = working_arr.pop().unwrap();
-            let next_elt_shift = range_gate.gate().select(ctx, elt_p, elt_n, itr_parity);
-            let true_next_elt = range_gate.gate().select(ctx, next_elt_shift, elt_c, not_rotated_enough);
-            shift_arr.push(true_next_elt);
-
-            working_arr.push(elt_p);
-            working_arr.push(elt_c);
-        }
-
-        working_arr.pop(); //remove the last "current element", working_arr is just [0]
-
-        if itr < 999 {
-            working_arr.extend(&shift_arr); // replacing working_arr
-            working_arr.push(ctx.load_zero()); //ending zero
-        } else { 
-            working_arr = shift_arr; // shift_arr is currently in reverse order
-            working_arr.reverse();
-        }
-
+        let rotate = range_gate.is_less_than(ctx, cur_iter, start, range_bits);
+        let rotate_int = rotate.value().to_u64_limbs(1, 1)[0];
+        working_arr = (0..1000).map(|idx| working_arr[ idx+ (rotate_int as usize)]).collect::<Vec<AssignedValue<F>>>();
+        working_arr.push(ctx.load_zero());
     };
+
+    working_arr.pop();
     println!("fin: {:?}", working_arr);
 
 }
